4d2331fd6f681b473d303d927ebae21b03e5b52e
Dmytro Kiperman <8boyss@ukr.net>
2023-11-28 21:57:26 +0100

    upd

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -60,5 +60,46 @@ class Slider {
     })
   }
 }
-
 Slider.init()
+
+class Header {
+  static #height = null
+  static #wrapper = null
+  static #button = null
+
+  static #isOpen = false
+
+  static init() {
+    this.#height = document.querySelector(
+      '.header__bottom',
+    ).offsetHeight
+
+    this.#wrapper = document.querySelector(
+      '.header__wrapper',
+    )
+    this.#button = document.querySelector('.header__button')
+
+    this.#button.onclick = this.#toggle
+  }
+
+  static #toggle = () => {
+    if (this.#isOpen) {
+      this.#button.classList.replace(
+        'header__button--close',
+        'header__button--open',
+      )
+
+      this.#wrapper.style.height = 0
+    } else {
+      this.#button.classList.replace(
+        'header__button--open',
+        'header__button--close',
+      )
+      this.#wrapper.style.height = `${this.#height}px`
+    }
+
+    this.#isOpen = !this.#isOpen
+  }
+}
+
+Header.init()
